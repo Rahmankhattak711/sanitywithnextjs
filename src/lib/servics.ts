f58e6b5aea70832187192fe47a8d183ef9090686
Rahmankhattak711 <rahmankhattak796@gmail.com>
2025-08-06 23:19:32 +0500

    feat : add split into component

--- src/lib/servics.ts
+++ src/lib/servics.ts
@@ -1,12 +1,44 @@
-export async function getBooks(setBooks: any, client: any) {
-  const query = `*[_type == "book"]{
-      _id,
-      title,
-      price,
-      description,
-      publishedAt,
-      "imageUrl": image.asset->url
-    }`;
-  const result = await client.fetch(query);
-  setBooks(result);
-}
+export const fetchData = async ({
+  slug,
+  setProductBlock,
+  setLoading,
+  client,
+  value,
+}: {
+  slug: any;
+  setProductBlock: any;
+  setLoading: any;
+  client: any;
+  value: string;
+}) => {
+  try {
+    if (!slug) return;
+
+    const data = await client.fetch(
+      `*[_type == "dynamicGenPage" && slug.current == $slug][0]{
+            body {
+              body[] {
+                _type,
+                _key,
+                title,
+                description,
+                price,
+                image {
+                  asset->{
+                    url
+                  }
+                }
+              }
+            }
+          }`,
+      { slug }
+    );
+
+    const product = data?.body?.body?.find((b: any) => b._type === value);
+    setProductBlock(product);
+  } catch (err) {
+    console.error("Error fetching product:", err);
+  } finally {
+    setLoading(false);
+  }
+};
